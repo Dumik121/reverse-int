08edcec97cd97f765e8c2b1a418a9db810aed54a
Dumik121 <nazar121duma@gmail.com>
2023-02-15 12:30:18 +0200

    feat: reverceInt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 module.exports = function reverse (n) {
-  
+    if(n.toString().includes("-"))n=n*(-1);
+  return (parseFloat(n.toString().split('').reverse().join('')) * Math.sign(n))                 
 }
